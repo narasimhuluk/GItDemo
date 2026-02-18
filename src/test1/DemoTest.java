10e22fe7931d37b0d4841d453cba96db65b64fc3
rahul <narasimhulu3k@gmail.com>
2026-02-18 11:57:28 +0530

    last commit

--- src/test1/DemoTest.java
+++ src/test1/DemoTest.java
@@ -7,21 +7,22 @@ public class DemoTest {
 		int a = 10;
 		int b = 20;
 		int total = a + b;
-		int multipication=a*b;
-		int division = a/b;
+		int multipication = a * b;
+		int division = a / b;
 		int multiplier = a * b;
 		System.out.println(total);
 		System.out.println(division);
 		System.out.println(multiplier);
 		System.out.println(multipication);
-		
-		
+
 	}
-	
-	public void getMethod()
-	{
-		int a=20;
-		int b=30;
+
+	public void getMethod() {
+		int a = 20;
+		int b = 30;
+
+		int c = a + b;
+		System.out.println(c);
 	}
 
 }
